dc38fefabc70612dabd04a9f5eb996e0abd057d1
oscwmuy <117035894+oscwmuy@users.noreply.github.com>
2024-10-23 13:12:29 +0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,34 @@
+const image = document.getElementById('music')
+const audio = document.querySelector('audio')
+changeImage()
+let isPlay = true
+
+function changeImage() {
+  if (!audio.paused) {
+    image.src = 'Media/icon-yes-sound.png'
+  } else {
+    image.src = 'Media/icon-no-sound.png'
+  }
+}
+
+const autoplay = function () {
+  if (isPlay) {
+    audio.play()
+    isPlay = false
+    changeImage()
+  }
+}
+
+document.addEventListener('click', autoplay)
+
+function toggleSound() {
+  if (audio.paused) {
+    audio.play()
+  } else {
+    audio.pause()
+  }
+
+  changeImage()
+}
+
+image.addEventListener('click', toggleSound)
